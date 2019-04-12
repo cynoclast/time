12ca128bfd90cc5e29663f1db053050b824b8d22
cynoclast <cynoclat@gmail.com>
2019-04-12 12:44:46 -0700

    Moves formats and pattern to static Changes methods to return String instead of printing directly

--- src/main/java/com/cynoclast/time/Time.java
+++ src/main/java/com/cynoclast/time/Time.java
@@ -4,26 +4,35 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.regex.Pattern;
 
 /**
  * @author cynoclast
  */
 @SuppressWarnings("Duplicates")
 public class Time {
+    private static final Pattern CONTAINS_AT_LEAST_ONE_DIGIT = Pattern.compile("\\d");
+
+    private static final DecimalFormat JUST_WHOLE_NUMBERS = new DecimalFormat("##");
+    private static final DecimalFormat JUST_FRACTION = new DecimalFormat(".##");
+
+    static {
+        JUST_WHOLE_NUMBERS.setRoundingMode(RoundingMode.DOWN);
+    }
 
     /**
      * Use cases:
-     *
+     * <p>
      * When to leave today:
-     *   8:00-1:00 1:30-
-     *
+     * 8:00-1:00 1:30-
+     * <p>
      * A day's time:
-     *
-     *  8:44-12:30 1:00-6:30
-     *
+     * <p>
+     * 8:44-12:30 1:00-6:30
+     * <p>
      * When to leave on Friday:
-     *
-     *   8.5 9.27 8.83 8.67 9:45-12:45 12:55-
+     * <p>
+     * 8.5 9.27 8.83 8.67 9:45-12:45 12:55-
      *
      * @param args one of the above inputs
      */
@@ -33,14 +42,29 @@ public class Time {
             System.exit(1);
         } else {
             if (args.length == 1 || args.length == 2) {
-                calculateOneDay(args);
+                final String timeToLeaveOutputString = calculateOneDay(args);
+                if (CONTAINS_AT_LEAST_ONE_DIGIT.matcher(timeToLeaveOutputString).find()) {
+                    // If there are numbers (indicating a time) output it
+                    System.out.println(timeToLeaveOutputString);
+                } else {
+                    // else it's an error message
+                    System.err.println(timeToLeaveOutputString);
+                    System.exit(1);
+                }
             } else if (args.length == 6) {
-                calculateFridayTimeToLeave(args);
+                final String fridayTimeToLeave = calculateFridayTimeToLeave(args);
+                System.out.println(fridayTimeToLeave);
             }
         }
     }
 
-    private static void calculateFridayTimeToLeave(String[] args) {
+    /**
+     * Calculates the time to leave on a Friday targeting 40.0 hours worked
+     *
+     * @param args the input string of four floats (hours for Monday-Thursday) and a morning start+end and afternoon start
+     * @return the time to leave in 24h time
+     */
+    private static String calculateFridayTimeToLeave(String[] args) {
         // 8.5 9.27 8.83 8.57 9:30-12:30 12:40-
         final Pair<Integer, Integer> morningDurations = calculateMorningDurations(args[4]);
 
@@ -54,10 +78,7 @@ public class Time {
         }
         int afternoonStartTimeMinute = Integer.parseInt(afternoonStartTimePieces[1]);
 
-        DecimalFormat df = new DecimalFormat("##");
-        df.setRoundingMode(RoundingMode.DOWN);
-
-        String hoursToGoString = df.format(hoursToGo);
+        String hoursToGoString = JUST_WHOLE_NUMBERS.format(hoursToGo);
         int afternoonStopTimeHour = afternoonStartTimeHour + Integer.parseInt(hoursToGoString);
 
         int afternoonStopTimeMinute = afternoonStartTimeMinute + morningDurations.getRight();
@@ -65,18 +86,12 @@ public class Time {
             afternoonStopTimeHour++;
             afternoonStopTimeMinute -= 60;
         }
-
-        System.out.println(afternoonStopTimeHour + ":" + afternoonStopTimeMinute);
-
-        if (hoursToGo > 1.0) {
-            System.out.println(hoursToGoString + " hours " + df.format(hoursToGo * 60) + " minutes");
-        } else {
-            System.out.println(new DecimalFormat("##").format(hoursToGo * 60) + " minutes");
-        }
+        return afternoonStopTimeHour + ":" + afternoonStopTimeMinute;
     }
 
     /**
      * Takes "8:00-1:00" and returns "5,0".
+     *
      * @param morningStartEndTimes the input time string
      * @return Integer pair of hours and minutes
      */
@@ -115,11 +130,13 @@ public class Time {
 
     /**
      * Input:
-     *  "8:35-1:00" Just morning
-     *  or "8:35-1:00 1:10-" Morning with unknown afternoon end time.
+     * "8:35-1:00" Just morning
+     * or "8:35-1:00 1:10-" Morning with unknown afternoon end time.
+     *
      * @param args The morning times or morning and afternoon start.
+     * @return time to leave output string for System.out-ing.
      */
-    private static void calculateOneDay(String[] args) {
+    private static String calculateOneDay(String[] args) {
         final Pair<Integer, Integer> morningDurations = calculateMorningDurations(args[0]);
 
         if (args.length == 1) {
@@ -151,15 +168,25 @@ public class Time {
             if (args[1].endsWith("-")) {
                 //8:35-1:00 1:10-
                 final Pair<Integer, String> integerStringPair = calculateTimeToLeave(args[1], timeLeftHours, timeLeftMinutes);
-                System.out.println("Time to leave: " + integerStringPair.getLeft() + ":" + integerStringPair.getRight());
+                return "Time to leave: " + integerStringPair.getLeft() + ":" + integerStringPair.getRight();
             } else {
                 //8:35-1:00 1:10-6:10
                 final float calculatedHoursForCompletedDay = calculateHoursForCompletedDay(args, morningDurations.getLeft(), morningDurations.getRight());
-                System.out.println(args[0] + " " + args[1] + " > " + calculatedHoursForCompletedDay);
+                return args[0] + " " + args[1] + " > " + calculatedHoursForCompletedDay;
             }
+        } else {
+            return "Need one or two arguments.";
         }
     }
 
+    /**
+     * Calculates the decimal hours for a day's work to two significant figures.
+     *
+     * @param args                   the day's log of time worked in roughly this format 8:35-1:00 1:20-5:10
+     * @param morningDurationHours   the number of hours worked in the morning
+     * @param morningDurationMinutes the numver of minutes worked in the morning
+     * @return the day's time in decimal hours
+     */
     static float calculateHoursForCompletedDay(String[] args, int morningDurationHours, int morningDurationMinutes) {
         //8:35-1:00 1:20-5:10
         final String[] afternoonStartTimePieces = args[1].split("-")[0].split(":");
@@ -192,21 +219,22 @@ public class Time {
         }
 
         float fractionalHoursWorked = minutesWorked / 60f;
-        DecimalFormat justFraction = new DecimalFormat(".##");
 
         // gross hack
-        String fractionalHoursWorkedrounded = hoursWorked + justFraction.format(fractionalHoursWorked);
+        String fractionalHoursWorkedrounded = hoursWorked + JUST_FRACTION.format(fractionalHoursWorked);
         fractionalHoursWorked = Float.parseFloat(fractionalHoursWorkedrounded);
         // /gross hack
-        return  fractionalHoursWorked;
+        return fractionalHoursWorked;
     }
 
     static Pair<Integer, String> calculateTimeToLeave(String args, int timeLeftHours, int timeLeftMinutes) {
         // 12:25-
         String afternoonStartTimeString = "";
         if (args.length() == 5) {
+            // ex: 1:30
             afternoonStartTimeString = args.substring(2, 4);
         } else if (args.length() == 6) {
+            // ex: 12:30
             afternoonStartTimeString = args.substring(3, 5);
         }
         if (!afternoonStartTimeString.matches("^[0-9]+$")) {
